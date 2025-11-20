e63f20c2924bf15e00eb72389dd13c24f816c342
sepehr khodadadi <98sobhan@gmail.com>
2025-11-20 20:57:13 +0000

    feat: add user serializers

---
+++ accounts/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username' , 'email', 'role', 'student_id', 'professor_id']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'role', 'student_id', 'professor_id']
+        extra_kwargs = {'password': {'write_only': True}}
+        def create(self, validated_data):
+            password = validated_data.pop("password")
+            user = User(**validated_data)
+            user.set_password(password)
+            user.save()
+            return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(username=data["username"], password=data["password"])
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        data["user"] = user
+        return data
+
