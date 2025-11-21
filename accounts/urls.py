3af92c525551c4f084dfdbb4cab50270abeeffdb
amir hossein ahmadi <94749676+amirhosseinahmadi4800@users.noreply.github.com>
2025-11-21 19:56:02 +0330

    update path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from accounts import views
+
+app_name = 'accounts'
 
 urlpatterns = [
-    
-    path('admin/' , admin.site.urls )
-    
+    path('admin/login/', views.admin_login, name='admin-login'),
+    path('admin/profile/', views.admin_profile, name='admin-profile'),
+    path('professors/', views.professor_list, name='professor-list'),
 ]
