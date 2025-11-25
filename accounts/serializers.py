c80dded5136f9359fcb713cd0f836c5a066ba978
sepehr khodadadi <98sobhan@gmail.com>
2025-11-25 20:03:35 +0000

    refactor(accounts): refactor LoginSerializer and delete RegisterSerialyzer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,23 +1,12 @@
 from rest_framework import serializers
-from .models import User
 from django.contrib.auth import authenticate
+from .models import User
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username' , 'email', 'role', 'student_id', 'professor_id']
-
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'role', 'student_id', 'professor_id']
-        extra_kwargs = {'password': {'write_only': True}}
-    def create(self, validated_data):
-        password = validated_data.pop("password")
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
+        fields = ['id', 'username', 'email', 'role', 'student_id', 'professor_id']
 
 
 class LoginSerializer(serializers.Serializer):
@@ -25,28 +14,20 @@ class LoginSerializer(serializers.Serializer):
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
-        user = authenticate(username=data["username"], password=data["password"])
+        username = data.get("username")
+        password = data.get("password")
+
+        if not username or not password:
+            raise serializers.ValidationError("Username and password are required")
+
+        user = authenticate(username=username, password=password)
+
         if not user:
-            raise serializers.ValidationError("Invalid credentials")
+            raise serializers.ValidationError("The username or password is incorrect")
+
+        if not user.is_active:
+            raise serializers.ValidationError("This account has been deactivated")
+
+        # رول رو همراه خروجی می‌دیم
         data["user"] = user
         return data
-
-
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "role", "student_id"]
-
-
-class ProfessorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "role", "professor_id"]
-
-
-class AdminSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "role"]
-
