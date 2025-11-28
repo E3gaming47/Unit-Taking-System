c9d56d324fccd6eda99943dc2c19ce504792867a
sepehr khodadadi <98sobhan@gmail.com>
2025-11-28 14:42:49 +0000

    feat(courses): add viewset for courses

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# courses/views.py
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from accounts.permissions import IsAdmin
+from .models import Course
+from .serializers import CourseSerializer
 
-# Create your views here.
+class CourseListCreateView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+class CourseRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    permission_classes = [IsAuthenticated, IsAdmin]
