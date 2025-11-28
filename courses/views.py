fb472542dcbdc3c42241733edb3af22709665ada
sepehr khodadadi <98sobhan@gmail.com>
2025-11-28 14:43:49 +0000

    feat(courses): add router for courses

--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,10 @@
-# courses/views.py
-from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from accounts.permissions import IsAdmin
 from .models import Course
 from .serializers import CourseSerializer
 
-class CourseListCreateView(generics.ListCreateAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-    permission_classes = [IsAuthenticated, IsAdmin]
-
-class CourseRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     permission_classes = [IsAuthenticated, IsAdmin]
