ca17815df3f386ba5576eba2a752602d79c74499
amir hossein ahmadi <94749676+amirhosseinahmadi4800@users.noreply.github.com>
2025-11-29 17:49:54 +0330

    Add Model Term

--- terms/models.py
+++ terms/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Term(models.Model):
+    name = models.CharField(
+        max_length=64,
+        unique=True,                  
+    )
+    
+    start_date = models.DateField()   # تاریخ شروع ترم
+    end_date = models.DateField()     # تاریخ پایان ترم
+
+    class Meta:
+        ordering = ["-start_date"]    # جدیدترین ترم‌ها اول نمایش داده شوند
+
+    def __str__(self):
+        return self.name              # نمایش خوانا در ادمین
+
+    def clean(self):         # جلوگیری از اینکه تاریخ شروع بعد از پایان شود
+
+        if self.start_date >= self.end_date:
+            raise ValidationError("start_date must be before end_date.") #این ورودی اشتباهه، اجازه نمی‌دم توی دیتابیس بره
