68b517c999d2c8259a27580435fbe8e1002b1907
amir hossein ahmadi <94749676+amirhosseinahmadi4800@users.noreply.github.com>
2025-12-04 20:14:12 +0330

    add-swagger

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import login_page, admin_departments, admin_courses
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Unit Taking System API",
+        default_version="v1",
+        description="سیستم جامع انتخاب واحد دانشگاهی - API Documentation",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@university.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),  # Changed to avoid conflict
@@ -12,4 +30,9 @@ urlpatterns = [
     path("api/accounts/", include("accounts.urls")),
     path("api/departments/", include("departments.urls")),
     path("api/courses/", include("courses.urls")),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
