dc91ba28fe72de0e3b5be0f5380632a3dd502e7c
sepehr khodadadi <98sobhan@gmail.com>
2025-12-15 21:12:11 +0000

    feat(offering):add Section, schedules, exam and prerequisite models

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
-from accounts.views import login_page, admin_departments, admin_courses, admin_students, admin_professors
-from accounts.views import login_page, admin_departments, admin_courses
-from django.urls import path, re_path
+from django.urls import include, path, re_path
+
+from accounts.views import (
+    admin_courses,
+    admin_departments,
+    admin_professors,
+    admin_students,
+    login_page,
+)
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
