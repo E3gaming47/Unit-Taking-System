117bca99494021c520dab2dd6278a3b542511dea
sepehr khodadadi <98sobhan@gmail.com>
2025-12-15 20:32:08 +0000

    feat(accounts):add dashboard for users

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,35 +1,36 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, AuthViewSet
-from .views import DepartmentsList
-from accounts.views import login_page
-from django.urls import path
 from .views import (
-    DepartmentsList,
-    LoginView,
-    CoursesList,
-    RegisterView,
-    SelectCourse,
-    RemoveCourse,
-    CourseDetail,
+    login_page,
+    dashboard_redirect,
+    admin_dashboard,
+    admin_departments,
+    admin_courses,
+    admin_students,
+    admin_professors,
+    student_dashboard,
+    professor_dashboard,
 )
-
+from .views import UserViewSet, AuthViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'auth', AuthViewSet, basename='auth')
 
 urlpatterns = [
-    # Router URLs
     path('', include(router.urls)),
-    path('login-pge/', login_page, name='login_page'),
-    path("departments/", DepartmentsList.as_view(), name="departments_list"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("list/", CoursesList.as_view(), name="courses_list"),
-    path("register/", RegisterView.as_view(), name="register"),
-    path("select/", SelectCourse.as_view(), name="select_course"),
-    path("remove/", RemoveCourse.as_view(), name="remove_course"),
-    path("<int:id>/detail/", CourseDetail.as_view(), name="course_detail"),
 
+    path('login-page/', login_page, name='login_page'),
 
+    path('/dashboard/', dashboard_redirect, name='dashboard-redirect'),
+
+    path('admin/dashboard/', admin_dashboard, name='admin-dashboard'),
+    path('admin/departments/', admin_departments, name='admin-departments'),
+    path('admin/courses/', admin_courses, name='admin-courses'),
+    path('admin/students/', admin_students, name='admin-students'),
+    path('admin/professors/', admin_professors, name='admin-professors'),
+
+    path('student/dashboard/', student_dashboard, name='student-dashboard'),
+
+    path('professor/dashboard/', professor_dashboard, name='professor-dashboard'),
 ]
